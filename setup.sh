918c7ac36ce54b8deb2ab481041b6dd423b53678
hanamaru <hanamaru@localhost.localdomain>
2025-01-31 16:51:36 -0300

    ADD: setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "X==========================================================X"
 echo "| ██████  ███████  ██████  ██████  ██████  ███████ ██████  |"
 echo "| ██   ██ ██      ██      ██    ██ ██   ██ ██      ██   ██ |"
@@ -8,3 +9,25 @@ echo "X=====================-by Hanamaru-========================X"
 echo "************************************************************"
 echo "*------------>| Welcome to Decoder Setup |<----------------*"
 echo "************************************************************"
+echo ""
+echo ""
+echo "Press Enter to Start Setup.........."
+read
+sleep 3
+echo "Creating folders...."
+sleep 3
+mkdir ~/decoder
+echo "Cloning script on folder....."
+sleep 3
+cp ./decoder.py ~/decoder/
+chmod +x ~/decoder/decoder.py
+sleep 3
+echo "Creating commands........."
+sleep 3
+echo "alias decoder='~/decoder/decoder.py'" >> ~/.bashrc
+source ~/.bashrc
+sleep 3
+echo "Thanks for use Decoder."
+sleep 3
+echo "Done."
+
